e3dedbb0c15312b5cbbb3958cf9c6d398c8a133c
김초향 <chkim9910@naver.com>
2024-01-19 18:20:29 +0900

    fixed untracked files

--- assets/js/calendar.js
+++ assets/js/calendar.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", function () {
   const calendarEl = document.getElementById("calendar");
   const calendar = new FullCalendar.Calendar(calendarEl, {
     initialView: "dayGridMonth",
-    contentHeight: 650,
+    // contentHeight: 650,
     aspectRatio: 2,
     headerToolbar: {
       start: "title", // will normally be on the left. if RTL, will be on the right
