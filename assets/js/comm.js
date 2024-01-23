0a543eda971fea45a748132790bb23c6d1aab306
김초향 <chkim9910@naver.com>
2024-01-23 17:11:08 +0900

    add swiper

--- assets/js/comm.js
+++ assets/js/comm.js
@@ -1,1 +1,41 @@
+// --------------------sub2--------------------
+var swiper = new Swiper(".swiper-more-chronological-list", {
+  slidesPerView: 2,
+  spaceBetween: 30,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
 
+// $(document).ready(function () {
+//   const poster = $(".poster-wrap");
+//   poster.slick({
+//     infinite: false,
+//     arrows: false,
+//     vertical: true,
+//     dots: true,
+//     speed: 1200,
+//     cssEase: "cubic-bezier(0.86, 0, 0.07, 1)",
+//   });
+
+//   // poster.mousewheel(function (e) {
+//   //   e.preventDefault();
+//   //   if (e.deltaY < 0) {
+//   //     if (
+//   //       $(this).slick("slickCurrentSlide") ===
+//   //       $(this).find(".slick-slide").length - 1
+//   //     ) {
+//   //       return;
+//   //     }
+
+//   //     $(this).slick("slickNext");
+//   //   } else {
+//   //     if ($(this).slick("slickCurrentSlide") === 0) {
+//   //       return;
+//   //     }
+
+//   //     $(this).slick("slickPrev");
+//   //   }
+//   // });
+// });
