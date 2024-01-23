8d2f489e8d7aeb4760fd723877d8e719cdabe064
heresue <sue.synpark@gmail.com>
2024-01-23 18:20:57 +0900

    main html 작업중

---
+++ assets/js/main.js
@@ -0,0 +1,10 @@
+
+
+// 공연정보 슬라이드
+var swiper = new Swiper(".swiper-performance", {
+    slidesPerView: "auto",
+    spaceBetween: 20, 
+    scrollbar: {
+      el: ".swiper-scrollbar",
+    },
+  });
