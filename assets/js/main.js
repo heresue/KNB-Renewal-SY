db09a6a6acfed53aee046f5d7d8074d7506aa426
heresue <sue.synpark@gmail.com>
2024-01-31 18:32:25 +0900

    main- html css

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,23 @@
+// main slide
+var swiper = new Swiper(".main-swiper", {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  autoplay: {
+    delay: 3500,
+  },
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+
+
 // 공연정보 슬라이드
 var swiper = new Swiper(".swiper-performance", {
   slidesPerView: "auto",
