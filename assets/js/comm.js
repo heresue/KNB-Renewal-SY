d03cc9f4e3fd7d285a52eba49ac37d4346701568
minji0164 <min016425@gmail.com>
2024-02-01 18:19:30 +0900

    sec-work 수정

--- assets/js/comm.js
+++ assets/js/comm.js
@@ -1,36 +1,41 @@
 
 // sub1-----------------------------------------------------------------------------------------------------------
-document.addEventListener("DOMContentLoaded", function () {
-  const sectionWork = document.querySelector(".sec-work");
-  const txtGoto = document.querySelector(".txt-goto");
+$(document).ready(function () {
+  const sectionWork = $(".sec-work");
+  const txtGoto = $(".txt-goto");
 
   // txt-goto를 숨김
-  txtGoto.style.opacity = "0";
-  txtGoto.style.visibility = "hidden";
+  txtGoto.css({ opacity: 0, visibility: "hidden" });
 
-  document.addEventListener("scroll", function () {
-    const scrollPosition = window.scrollY;
-    const { offsetTop: sectionWorkTop, offsetHeight: sectionWorkHeight } =
-      sectionWork;
-    const windowHeight = window.innerHeight;
+  $(document).scroll(function () {
+      const scrollPosition = $(window).scrollTop();
+      const sectionWorkTop = sectionWork.offset().top;
+      const sectionWorkHeight = sectionWork.outerHeight();
+      const windowHeight = $(window).height();
 
-    // txt-goto가 sec-work에 도달하면 나타나게
-    if (
-      scrollPosition >= sectionWorkTop &&
-      scrollPosition <= sectionWorkTop + sectionWorkHeight - windowHeight
-    ) {
-      txtGoto.style.opacity = "1";
-      txtGoto.style.visibility = "visible";
-      // txt-goto 화면 중앙에 위치
-      txtGoto.style.top = `${(windowHeight - txtGoto.offsetHeight) / 2}px`;
-    } else {
-      // 스크롤 벗어나면 txt-goto 다시 숨김
-      txtGoto.style.opacity = "0";
-      txtGoto.style.visibility = "hidden";
-    }
+      // txt-goto가 sec-work에 도달하면 나타나게
+      if (
+          scrollPosition >= sectionWorkTop &&
+          scrollPosition <= sectionWorkTop + sectionWorkHeight - windowHeight
+      ) {
+          txtGoto.css({ opacity: 1, visibility: "visible" });
+          // txt-goto 화면 중앙에 위치
+          const topValue = (windowHeight - txtGoto.outerHeight()) / 2;
+          txtGoto.css({ top: `${topValue}px` });
+      } else {
+          // 스크롤 벗어나면 txt-goto 다시 숨김
+          txtGoto.css({ opacity: 0, visibility: "hidden" });
+      }
   });
 
+  AOS.init({
+      duration: 1000,
+  });
+});
+
+
   //sidebar-------------------------------------------------
+document.addEventListener("DOMContentLoaded", function () {
   $(".smoothscroll").on("click", function (e) {
     e.preventDefault();
 
@@ -49,10 +54,6 @@ document.addEventListener("DOMContentLoaded", function () {
           window.location.hash = target;
         }
       );
-  });
-
-  AOS.init({
-    duration: 1000,
   });
 });
 
